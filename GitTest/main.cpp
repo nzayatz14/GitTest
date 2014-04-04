8b4571801e07fd8f2d4c759a3d167e494050015f
nzayatz14 <nzayatz14@hotmail.com>
2014-04-04 11:57:47 -0400

    Initial Commit

---
+++ GitTest/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  GitTest
+//
+//  Created by Nick Zayatz on 4/4/14.
+//  Copyright (c) 2014 Nick Zayatz. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
